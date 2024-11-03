02eda287b17186ac0f86fad6ac2554b5637162ba
ligancat <ligancat@gmail.com>
2024-11-03 18:52:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ async function runIteration(iteration) {
 
     // Wait before taking a full-page screenshot
     await setTimeout(4000); // Wait for 4 seconds before taking a screenshot
-    await page.screenshot({ path: `screenshots/screenshot-${iteration}.jpg`, type: 'jpeg', fullPage: true });
+    await page.screenshot({ path: `screenshot-${iteration}.jpg`, type: 'jpeg', fullPage: true });
     
     console.log(`Completed iteration ${iteration}/2`);
     await browser.close();
